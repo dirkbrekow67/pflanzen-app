ed65162a762b6dd368c4cf73a2c7ce2cc6e0f00d
Dirk Brekow <dirk.brekow@yahoo.de>
2026-05-02 14:57:16 +0200

    Monatsliste zentral auslagern

--- pflanzen-app/src/components/PotForm.jsx
+++ pflanzen-app/src/components/PotForm.jsx
@@ -1,17 +1,4 @@
-const months = [
-  { label: "Januar", value: 1 },
-  { label: "Februar", value: 2 },
-  { label: "März", value: 3 },
-  { label: "April", value: 4 },
-  { label: "Mai", value: 5 },
-  { label: "Juni", value: 6 },
-  { label: "Juli", value: 7 },
-  { label: "August", value: 8 },
-  { label: "September", value: 9 },
-  { label: "Oktober", value: 10 },
-  { label: "November", value: 11 },
-  { label: "Dezember", value: 12 },
-];
+import { months } from "../constants/months";
 
 function PotForm({
   formData,
