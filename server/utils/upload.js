df255069b8b7c1a3d1994af7aeea0a2f15e8b34b
Dirk Brekow <dirk.brekow@yahoo.de>
2026-05-11 19:04:34 +0200

    Separate upload directories

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,9 +1,23 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "server/uploads/");
+    let targetFolder = "server/uploads/";
+
+    if (req.originalUrl.includes("seed-profile-photos")) {
+      targetFolder = "server/uploads/seed-profiles/";
+    }
+
+    if (req.originalUrl.includes("/api/photos")) {
+      targetFolder = "server/uploads/pots/";
+    }
+
+    if (!fs.existsSync(targetFolder)) {
+      fs.mkdirSync(targetFolder, { recursive: true });
+    }
+    cb(null, targetFolder);
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
